ff3af159598fbff8e1b4dee9ce51c13333626535
shwetha-DabKick <shwetha@dabkick.com>
2018-11-19 13:33:54 +0530

    Updated with latest demo app code

--- app/src/main/java/sample/sdk/dabkick/livechat/PartnerApplication.java
+++ app/src/main/java/sample/sdk/dabkick/livechat/PartnerApplication.java
@@ -1,6 +1,8 @@
 package sample.sdk.dabkick.livechat;
 
+import android.app.Application;
+
 import com.dabkick.engine.Public.DKVPApp;
 
-public class PartnerApplication extends DKVPApp {
+public class PartnerApplication extends Application {
 }
